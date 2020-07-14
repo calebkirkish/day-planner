815fb7adcc06b655b2aa6bae08ea3e5ff80a328c
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-07-14 00:17:43 -0700

    basic setup of the website

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+// get the date
+    // check if the date already has an entry in LS
+    // If it is then set the variable to the object and populate the table with the object
+    // If it isnt then set set it and creat a new object with empty strings
+    // When a user edits a field turn conteneditable to true
+    // when a user saves add the data to the object and set it in local storage again
