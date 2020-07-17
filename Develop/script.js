1a79d15239f0fff9a58198d4cb7b3eb2c7c15ee3
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-07-17 11:18:50 -0700

    changed some ID's as well as some other elements in the index, got started with a click event in the script.js, added some styling to the css

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,9 @@
     // If it isnt then set set it and creat a new object with empty strings
     // When a user edits a field turn conteneditable to true
     // when a user saves add the data to the object and set it in local storage again
+
+
+    $("#9am").on("click", function(){
+        console.log("Hello");
+        console.log($("#9am-text").val())
+    })
