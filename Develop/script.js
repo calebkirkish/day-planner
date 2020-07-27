f881d7e9e941314afd1dec4fe4c067d2882fc077
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-07-27 13:27:58 -0700

    added icons to the save button, enhanced the look of the table, added some functions to add a time

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,9 @@
     // When a user edits a field turn conteneditable to true
     // when a user saves add the data to the object and set it in local storage again
 
-
+    let now = moment();
+    console.log(now.format());
+    // When I click on the cell-save cell do this:
     $(".cell-save").on("click", function(){
         // $(this) = button
         var time = $(this).attr('id');
@@ -27,6 +29,6 @@
         
         var value = window.localStorage.getItem(key);
         $(key).text(value);
-        console.log(key);
-        console.log(value);
+        // console.log(key);
+        // console.log(value);
     });
